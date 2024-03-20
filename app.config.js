73c32771a89c7d4a51338b623a0a563c3e416e1b
JarredNKalina <jarredkalina@gmail.com>
2024-03-20 01:52:05 -0400

    idk whats going on

--- app.config.js
+++ app.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-	name: 'Columbiana Barber Shop',
-	version: '1.0.0',
-	extra: {
-	  clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+	name: "Columbiana Barber Shop",
+	version: "1.0.0",
+	splash: {
+		image: "./assets/splash.png",
+		resizeMode: "contain",
+		backgroundColor: "#ffffff",
 	},
-  };
+	assetBundlePatterns: ["**/*"],
+	extra: {
+		clerkPublishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+		eas: {
+			projectId: "d5a6c68d-aaf9-4661-a59f-953b0559a71c",
+		},
+	},
+	runtimeVersion: "1.0.0",
+}
